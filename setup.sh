0c89d93879741d06990c2546ed40ba4e1f349e06
Nate Bailey <nathanaelbailey@gmail.com>
2019-12-11 22:20:06 -0800

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+mkdir /usr/local/share/antigen
+curl -L git.io/antigen > /usr/local/share/antigen/antigen.zsh
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+brew install neovim iterm2
+npm install -g spaceship-prompt
 cp ./.gitconfig ~/.gitconfig
 cp ./.zshrc ~/.zshrc
 cp ./.vimrc ~/.vimrc
-cp ./.vimrc_background ~/.vimrc_background
-cp ./.nvimrc ~/.config/nvim/init.rc
+cp ./.nvimrc ~/.config/nvim/init.vim
